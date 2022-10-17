dd529fc828d36be991722b24fde221c0f3ed22d0
Tiago Cândido do Vale <tiagocandidodovale@MacBook-Pro-de-Tiago.local>
2022-10-17 19:01:34 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fruits/src/app_module.dart';
+import 'package:fruits/src/app_widget.dart';
+
+void main() {
+  return runApp(
+    ModularApp(module: AppModule(), child: const AppWidget()),
+  );
+}
---
+++ lib/src/modules/home/page/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 110, 170, 112),
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
+
